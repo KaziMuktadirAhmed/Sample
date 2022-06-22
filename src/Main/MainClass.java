3b36eb7434a3832bad7e505d278dcd005ee87bf6
kazimuktadir <bsse1111@iit.du.ac.bd>
2022-06-22 13:05:53 +0600

    initial commit

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -3,5 +3,15 @@ package Main;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("hello world");
+        func();
+    }
+
+    public static void func() {
+        System.out.println("printing something");
+        int oka = 36;
+        System.out.println("okay printing");
+        for(int i=0; i<oka; i++){
+            System.out.println(i);
+        }
     }
 }
