666601886ecfd47b4093813b6fd233afbeafab88
Kazi Muktadir Ahmed <kazisujoy@gmail.com>
2022-08-02 11:31:54 +0600

    initial commit

---
+++ src/Parser/CppParser.java
@@ -0,0 +1,13 @@
+package Parser;
+
+import java.io.File;
+
+public class CppParser {
+    private final File inputFile;
+
+    public CppParser(String inputFilePath) {
+        this.inputFile = new File(inputFilePath);
+    }
+
+
+}
